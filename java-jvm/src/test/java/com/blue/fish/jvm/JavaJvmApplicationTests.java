9536e86ee4fea3d67931c84558d54e5a3a6920a3
chenlanqing <743633145@qq.com>
2018-11-19 22:11:27 +0800

    add jvm module

---
+++ java-jvm/src/test/java/com/blue/fish/jvm/JavaJvmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blue.fish.jvm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaJvmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
