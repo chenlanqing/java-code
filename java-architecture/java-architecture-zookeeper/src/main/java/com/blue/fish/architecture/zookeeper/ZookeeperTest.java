a1e771abfc8342eaad71a4d85fbddca3cbb77711
chenlanqing <743633145@qq.com>
2018-11-19 22:43:58 +0800

    add design pattern and architecture module

---
+++ java-architecture/java-architecture-zookeeper/src/main/java/com/blue/fish/architecture/zookeeper/ZookeeperTest.java
@@ -0,0 +1,8 @@
+package com.blue.fish.architecture.zookeeper;
+
+/**
+ * @author bluefish 2018/11/19
+ * @version 1.0.0
+ */
+public class ZookeeperTest {
+}
---
+++ java-design/java-design-pattern/src/main/java/com/blue/fish/design/pattern/creational/simplefactory/Video.java
@@ -0,0 +1,8 @@
+package com.blue.fish.design.pattern.creational.simplefactory;
+
+/**
+ * @author bluefish 2018/11/19
+ * @version 1.0.0
+ */
+public class Video {
+}
