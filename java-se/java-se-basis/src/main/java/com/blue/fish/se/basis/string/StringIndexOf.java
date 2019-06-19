20b2faa60857fe672d9e66dccefcd57f03b322ce
chenlanqing <743633145@qq.com>
2019-06-19 22:27:33 +0800

    add system design module

--- java-se/java-se-basis/src/main/java/com/blue/fish/se/basis/string/StringIndexOf.java
+++ java-se/java-se-basis/src/main/java/com/blue/fish/se/basis/string/StringIndexOf.java
@@ -1,5 +1,7 @@
 package com.blue.fish.se.basis.string;
 
+import java.util.Arrays;
+
 /**
  * @author bluefish 2019-05-16
  * @version 1.0.0
@@ -8,9 +10,9 @@ public class StringIndexOf {
 
     public static void main(String[] args) {
 
-        String s = "abcdefghijk";
-
-        System.out.println(s.indexOf("cde"));
+//        String[] split = split("a|b|c", "|");
+        String[] split = "a|b|c".split("\\|");
+        System.out.println(Arrays.toString(split));
 
     }
 }
--- java-se/java-se-thread/src/main/java/com/blue/fish/se/thread/base/ThreadJoin.java
+++ java-se/java-se-thread/src/main/java/com/blue/fish/se/thread/base/ThreadJoin.java
@@ -14,7 +14,6 @@ public class ThreadJoin {
         Thread t3 = new Thread(yield, "t3");
 
         t1.start();
-        t1.join();
         t2.start();
         t2.join();
         t3.start();
