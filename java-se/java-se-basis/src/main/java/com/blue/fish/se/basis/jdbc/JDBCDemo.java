ebd156f511accd98ea6857f7f410256cf2d219d5
chenlanqing <743633145@qq.com>
2019-06-24 11:09:30 +0800

    origin jdbc

---
+++ java-se/java-se-basis/src/main/java/com/blue/fish/se/basis/jdbc/JDBCDemo.java
@@ -0,0 +1,40 @@
+package com.blue.fish.se.basis.jdbc;
+
+import com.mysql.jdbc.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author bluefish 2019-06-24
+ * @version 1.0.0
+ */
+public class JDBCDemo {
+
+    public static void main(String[] args) throws Exception {
+        DriverManager.registerDriver(new Driver());
+
+        String url = "jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=utf-8&allowMultiQueries=true";
+        String userName = "root";
+        String password = "root";
+
+        Connection connection = DriverManager.getConnection(url, userName, password);
+
+        String sql = "select ename from t_emp where sal < ? and job = ? ";
+
+        PreparedStatement ps = connection.prepareStatement(sql);
+        ps.setObject(1, "1500");
+        ps.setObject(2, "SALESMAN");
+
+        ResultSet resultSet = ps.executeQuery();
+
+        while (resultSet.next()) {
+            String ename = resultSet.getString("ename");
+            System.out.println(ename);
+        }
+
+        connection.close();
+    }
+}
