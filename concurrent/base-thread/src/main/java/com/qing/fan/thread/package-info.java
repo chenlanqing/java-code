99b27e630ce440e2f52e30a0eb67586780106c91
chenlanqing <743633145@qq.com>
2024-11-03 15:56:30 +0800

    add module

---
+++ concurrent/base-thread/src/main/java/com/qing/fan/thread/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2024年11月03日 15:50
+ */
+package com.qing.fan.thread;
---
+++ file/src/main/java/com/qing/fan/file/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2024年11月03日 15:54
+ */
+package com.qing.fan.file;
