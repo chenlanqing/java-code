ea8dff54289018e45e55630788089669bdd142a0
chenlanqing <743633145@qq.com>
2024-11-03 15:57:38 +0800

    remove module

--- file/src/main/java/com/qing/fan/file/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author QingFan
- * @version 1.0.0
- * @date 2024年11月03日 15:54
- */
-package com.qing.fan.file;
--- socket/src/main/java/com/qing/fan/socket/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @author QingFan
- * @version 1.0.0
- * @date 2024年11月03日 15:52
- */
-package com.qing.fan.socket;
